e626763cb1df9a27138ecc5c7b1bf4c85bcdc3cc
Albert Zhang <zzy0064@gmail.com>
2024-06-18 17:20:26 +1200

    add tailwind into project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', "@nuxtjs/tailwindcss"],
   routeRules: {
     '/': { prerender: true }
   }
